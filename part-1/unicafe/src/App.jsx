8d18268d57e5072cf9f3a2947f49e040382a47ce
AnjaP <treyjapan@pm.me>
2024-01-04 22:31:52 +0100

    finish exercise-1-9 unicafe

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -1,36 +1,36 @@
-import { useState } from 'react';
-import Statistics from './components/Statistics';
+import { useState } from "react";
+import Statistics from "./components/Statistics";
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const handleClick = (feedback) => {
     const handleFeedBack = () => {
-      if (feedback === 'good') {
-      setGood(good +1);
-    } else if (feedback === 'neutral') {
-      setNeutral(neutral + 1);
-    } else if (feedback === 'bad') {
-      setBad(bad + 1);
-    }
-   }
-   return handleFeedBack }
+      if (feedback === "good") {
+        setGood(good + 1);
+      } else if (feedback === "neutral") {
+        setNeutral(neutral + 1);
+      } else if (feedback === "bad") {
+        setBad(bad + 1);
+      }
+    };
+    return handleFeedBack;
+  };
 
   return (
     <main>
       <h1>Give Feedback</h1>
       <div>
-      <button onClick={handleClick('good')}>good</button>
-      <button onClick={handleClick('neutral') }>neutral</button>
-      <button onClick={handleClick('bad') }>bad</button>
+        <button onClick={handleClick("good")}>good</button>
+        <button onClick={handleClick("neutral")}>neutral</button>
+        <button onClick={handleClick("bad")}>bad</button>
       </div>
-    <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </main>
-  )
-}
+  );
+};
 
 export default App;
-
--- part-1/unicafe/src/components/Statistics.jsx
+++ part-1/unicafe/src/components/Statistics.jsx
@@ -1,19 +1,24 @@
 const Statistics = ({ good, neutral, bad }) => {
-    const total = good + neutral + bad;
+  const total = good + neutral + bad;
 
   return (
     <div>
-        <h2>Statistics</h2>
-      <div>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {total}</p>
-        <p>average {(good - bad) / total}</p>
-        <p>positive {good / total * 100}%</p>
-      </div>
+      <h2>Statistics</h2>
+
+      {total > 0 ? (
+        <div>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+          <p>all {total}</p>
+          <p>average {(good - bad) / total}</p>
+          <p>positive {(good / total) * 100}%</p>
+        </div>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Statistics;
